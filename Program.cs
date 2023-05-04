5972389efd2b6795222e77fc263c78693e052564
agilsonaraujoo <agilsonaraujo2828@gmail.com>
2023-05-04 11:32:05 -0300

    curso DIO C#

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using exemplo.models;
+
+// // cast - casting (converte os tipos de stings, ints, booleans, etc...)
+// string a = Convert.ToString("Agilson");
+
+// Console.WriteLine(a);
+
+int a = 5;
+string b = a.ToString();
+
+Console.WriteLine(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CTRL + K +C 
+
+// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+
+// c *= 5;
+
+// Console.WriteLine(c);
+
